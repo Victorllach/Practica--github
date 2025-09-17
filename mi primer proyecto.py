3a3f0e786ac284dc5dbf30de2c3d039ce1b56dd5
Victorllach <victor.llach@campus.xaloc.org>
2025-09-17 09:14:31 +0200

    Add files via upload

---
+++ mi primer proyecto.py
@@ -0,0 +1,39 @@
+# En este apartado estamos definiendo variables
+var1="xaloc"   # esta variable es de tipo string y como puedes observar esta entre comillas
+var2=4
+var3=5
+var4=1.8  #esta variable contiene un valor de tipo float/decimal
+
+# este es un ejemplo de como no debo definir una variable var3= "no es correcto"
+print=("Hola mi nombre es victor")
+suma=var2+var3
+print(suma)
+print(var2+var3)
+print("el resultado de la suma de var2+var3 es ")
+#A continuacion hacemos uso de la funcion input, instruccion que permite capturar
+#un valor introducido por el usuario via teclado
+#recuerda que en la instuccion input podemos introducir mensajes informativos
+#siempre entre comillas " "
+nombre=input()
+apellido1=input("introduce tu primer apellido ")
+apellido2=input("introduce tu segundo apellido ")
+#He definido una nueva variable nombre_completo (recuerda que las variables no
+#esta variable contiene la concatencion/union de las tres variables y ademas
+#he introducido unas " " entre cada una de ellas para que exista un espacio en
+#y el contenido no sea todo unido
+nombre completo= nombre + " " + apellido1 + " " + apellido2
+#finalmente utilizamos la funcion print para visualizar por pantalla el contenido
+#en este caso de la variable nombre_completo
+print(nombre_completo)
+suma=var2+var3
+print(f"la suma de {var2} + {var3} es igual a ", suma)
+resta=var2+var3
+multiplicacion=var2*var3
+print(f"la multiplicacion de {var2} * {var3} es igual a ",multiplicacion)
+division=var2/var3
+print(f"La division entera de {var2} / {var3} es igual a ", division)
+division_entera=var2//var3
+print(f"el resto de la division de {var2} /{var3} es igual a ", division_entera)
+modulo_division=var2%var3
+print(f"el resto de la division de {var2} / {var3} es igual a " modulo_division)
+print(f"la potencia de {var2} 
